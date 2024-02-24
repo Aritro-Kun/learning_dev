97d769ff0dc433bb127aca31bb1c7e8d40d44725
Aritro Kundu <149681607+Aritro-Kun@users.noreply.github.com>
2024-02-24 13:17:20 +0530

    helloWorldExpress.js

--- helloWorldExpress.js
+++ helloWorldExpress.js
@@ -1,18 +1,16 @@
-
-const express = require ('express');
+const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get("/route-handler", function(req, res) {
-    res.json({
-        name:'Aritro',
-        age: '18'
-    })
+app.get('/aritro', function(req, res) {
+  res.json({
+    name:'Aritro',
+    age:19
+  })
 })
 
-app.get('/', function(req, res){
-  res.send('Hello World!');
+app.get('/', function(req, res) {
+  res.send('Hello World');
 })
 
 app.listen(port);
-
